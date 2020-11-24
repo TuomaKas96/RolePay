99d66a50d24eb65a1faf0bac7f060fb1b20ee9d9
Zerahar <heinisaastamoinen@gmail.com>
2020-11-24 13:49:43 +0200

    Changed activities into fragments. Added navigation. Moved MainActivity contents into StartView.

--- app/src/main/java/com/example/rolepay/UserBalance.kt
+++ app/src/main/java/com/example/rolepay/UserBalance.kt
@@ -1,11 +1,18 @@
 package com.example.rolepay
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class UserBalance : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_balance)
+class UserBalance : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.activity_user_balance, container, false)
     }
 }
--- app/src/main/java/com/example/rolepay/UserMainView.kt
+++ app/src/main/java/com/example/rolepay/UserMainView.kt
@@ -1,11 +1,28 @@
 package com.example.rolepay
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 
-class UserMainView : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_main_view)
+class UserMainView : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.activity_user_main_view, container, false)
+        val requestBtn = v.findViewById(R.id.new_request_button) as Button
+        requestBtn.setOnClickListener {
+            NavHostFragment.findNavController(this).navigate(R.id.newRequest)
+        }
+        val paymentBtn = v.findViewById(R.id.new_payment_button) as Button
+        paymentBtn.setOnClickListener {
+            NavHostFragment.findNavController(this).navigate(R.id.newPayment)
+        }
+        return v
     }
 }
