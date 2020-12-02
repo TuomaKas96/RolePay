46db1d0c2f759e10655260fb295361ef480d4280
Tuomas <tuomas.kasanen@edu.savonia.fi>
2020-12-02 16:17:49 +0200

    Login works! Public token is also visible now in UserMainView.kt

--- app/src/main/java/com/example/rolepay/StartView.kt
+++ app/src/main/java/com/example/rolepay/StartView.kt
@@ -14,9 +14,11 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import com.google.gson.JsonParser
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_start_view.*
 import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 
 class StartView : Fragment() {
@@ -41,6 +43,8 @@ class StartView : Fragment() {
 
             loader.visibility = View.VISIBLE //makes the loader spinny thing visible after pressing the button
 
+            //TODO: maybe turn the code below into a separate function?
+
             // Create an intent
             val i = Intent()
             // Create parameters, they will be added to the url/request body
@@ -59,25 +63,68 @@ class StartView : Fragment() {
                     super.onReceiveResult(resultCode, resultData)
                     // Successful API call
                     if (resultCode == 200) {
-                        Log.d("SubApplication", "Data retrieved: " + resultData.getString("Data"))
+                        Log.d("StartView", "Data retrieved: " + resultData.getString("Data"))
 
-                        //hide the loader after the query
-                        loader.visibility = View.INVISIBLE
-                        //hide error message if visible from previous attempts
-                        if (errorText.visibility == View.VISIBLE){
-                            errorText.visibility = View.INVISIBLE
+                        //check for errors in data
+                        var error = false
+                        try{
+                            //parse data
+                            val jsonArray = JSONArray(resultData.getString("Data"))
+                            val jsonObject: JSONObject = jsonArray.getJSONObject(0)
+
+                            //this is how you get a specific value from the JSON object
+                            //val userID = jsonObject.get("user_id")
+                        }
+                        catch(e: JSONException){
+                            //log the exception and set error as true
+                            Log.d("StartView", "Error while logging in: " + e.toString())
+                            error = true
                         }
 
-                        //TODO: parse resultData and add data to session or some such
-                        //Log.d("StartView", "User id: " +  userID)
+                        //login failed if errors in data
+                        if(error){
+                            //hide the loader
+                            loader.visibility = View.INVISIBLE
 
-                        //TODO: Check whether it is an admin or a user that logins. If admin, use "R.id.loginAsAdmin"
-                        //navigate to user_main_view
-                        NavHostFragment.findNavController(this@StartView).navigate(R.id.loginAsUser)
+                            //show error message after failed API call
+                            errorText.visibility = View.VISIBLE
+                        }
+                        //otherwise complete login with given data
+                        else{
+                            val jsonArray = JSONArray(resultData.getString("Data"))
+                            val jsonObject: JSONObject = jsonArray.getJSONObject(0)
+
+                            //add data to storage for later use
+                            SubApplication.userId = jsonObject.get("user_id") as Int?
+                            SubApplication.publicToken = jsonObject.get("public_token") as String?
+                            SubApplication.privateToken = jsonObject.get("private_token") as String?
+                            SubApplication.admin = jsonObject.get("admin") as Int?
+                            SubApplication.balanceId = jsonObject.get("balance_id") as Int?
+                            SubApplication.environmentId = jsonObject.get("environment_id") as Int?
+
+                            //hide the loader after the query
+                            loader.visibility = View.INVISIBLE
+                            //hide error message if visible from previous attempts
+                            if (errorText.visibility == View.VISIBLE){
+                                errorText.visibility = View.INVISIBLE
+                            }
+
+                            //Check whether it is an admin or a user that logins.
+                            if(jsonObject.get("admin") == 0){
+                                //navigate to user_main_view
+                                NavHostFragment.findNavController(this@StartView).navigate(R.id.loginAsUser)
+                            }
+                            else{
+                                //navigate to user_main_view as admin
+                                NavHostFragment.findNavController(this@StartView).navigate(R.id.loginAsAdmin)
+                            }
+                        }
+
+
                     } else { // Failed API call
                         Log.i("SubApplication", "Something went wrong: " + resultData.getString("Error"))
 
-                        //hide the loader after the query
+                        //hide the loader
                         loader.visibility = View.INVISIBLE
 
                         //show error message after failed API call
--- app/src/main/java/com/example/rolepay/UserMainView.kt
+++ app/src/main/java/com/example/rolepay/UserMainView.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 
@@ -16,10 +17,21 @@ class UserMainView : Fragment() {
     ): View? {
         val v = inflater.inflate(R.layout.activity_user_main_view, container, false)
 
+        //TODO: add logout button and functionality.
+        // lock the "back" button so user can't return to start view unless logging out
+
+        //TODO: show balance value and add navigation to balance view (onClick for balance value)
+
+        //sets public token text as the one from storage
+        val publicTokenValue = v.findViewById(R.id.public_token_val_text) as TextView
+        publicTokenValue.text = SubApplication.publicToken.toString()
+
+        //navigate to new request
         val requestBtn = v.findViewById(R.id.new_request_button) as Button
         requestBtn.setOnClickListener {
             NavHostFragment.findNavController(this).navigate(R.id.newRequest)
         }
+        //navigate to new payment
         val paymentBtn = v.findViewById(R.id.new_payment_button) as Button
         paymentBtn.setOnClickListener {
             NavHostFragment.findNavController(this).navigate(R.id.newPayment)
